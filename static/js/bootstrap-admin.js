efbf52d9b05d11ef02eec2806d405bebd69018c4
x852 <lujiahao@88.com>
2022-07-13 15:26:28 +0800

    temp save

--- static/js/bootstrap-admin.js
+++ static/js/bootstrap-admin.js
@@ -1,4 +1,20 @@
 ;(function () {
+
+
+    let scrollTop;
+
+    function lockScroll() {
+        scrollTop = document.scrollingElement.scrollTop;
+        document.body.classList.add('bsa-lock-scroll');
+        document.body.style.top = -scrollTop + 'px';
+    }
+
+    function unLockScroll() {
+        document.body.classList.remove('bsa-lock-scroll');
+        document.scrollingElement.scrollTop = scrollTop;
+    }
+
+
     function siblings(el) {
         let a = [];    //保存所有兄弟节点
         let p = el.parentNode.children; //获取父级的所有子节点
@@ -44,84 +60,174 @@
     window.addEventListener("DOMContentLoaded", function () {
 
 
-        let bodyEl = document.body;
-        let bsAdminSidebarWrapper = document.querySelector('.bs-admin-sidebar .bs-admin-sidebar-wrapper');
-        let bsAdminSidebar = document.querySelector('.bs-admin-sidebar');
-        let bsAdminMain = document.querySelector('.bs-admin-main');
-
-        let bsAdminSidebarMobileToggle = document.querySelector('.bs-admin-sidebar-mobile-toggle');
-        let bsAdminNavbarMobileToggle = document.querySelector('.bs-admin-navbar-mobile-toggle');
+        let msk = 'bsm-mask';
 
 
-        let bsAdminMaskClassName = 'bs-admin-mask';
+        let eqwewqweq = document.querySelector('.mobile-toggle-menu');
 
-        let bsAdminSwitcherBtn = document.querySelector('.bs-admin-switcher-btn');
+        let sidebarEl = document.querySelector('.bsm-sidebar');
+        let bsAdminSidebarWrapper = document.querySelector('.bsm-sidebar-body');
 
-        bsAdminSidebar.style.transition = 'transform 300ms';
-        bsAdminMain.style.transition = 'margin-left 300ms';
-        bsAdminMain.insertAdjacentHTML('beforeEnd', `<iframe></iframe>`);
-        let iframe = document.querySelector('.bs-admin-main iframe');
-        let aDataDefaultPage = document.querySelector('a[data-defaultpage]');
-        let bsAdminHeaderColorWrap = document.querySelector('.bs-admin-headercolor-wrap');
-        let bsAdminSidebarColorWrap = document.querySelector('.bs-admin-sidebarcolor-wrap');
-        aDataDefaultPage && iframe.setAttribute('src', aDataDefaultPage.getAttribute('href'));
-        const bsAdminNavbarCollapse = new bootstrap.Collapse('.bs-admin-navbar-collapse', {
-            toggle: false
-        });
-        const bsAdminSwitcherOffcanvas = new bootstrap.Offcanvas(document.querySelector('.bs-admin-switcher-offcanvas'));
 
-        bsAdminSwitcherBtn.addEventListener('click', function (event) {
-            event.preventDefault();
-            bsAdminSwitcherOffcanvas.show();
-        });
+        let timer = null; //定义一个定时器
 
-        //监听所有的特定属性的a链接
-        eventDelegate(bodyEl, 'click', 'a[data-target-iframe]:not(.has-children)', function (event) {
+
+        let enableClick = true;
+
+
+        //回到顶部事件委托
+        eventDelegate(document.body, 'click', '.back-to-top', function (event) {
             event.preventDefault();
             event.stopPropagation();
-            let url = this.getAttribute('href') + "?var=" + Math.random();
-            iframe.setAttribute('src', url)
+
+            if (enableClick === true) {
+                enableClick = false;
+                //设置一个定时器
+                timer = setInterval(function () {
+
+                    let osTop = document.documentElement.scrollTop || document.body.scrollTop;
+                    let speed = Math.floor(-osTop / 6);
+                    document.documentElement.scrollTop = document.body.scrollTop = osTop + speed;
+
+                    if (osTop === 0) {
+                        enableClick = true;
+                        clearInterval(timer);
+                    }
+
+                }, 16);
+
+            }
         });
-        bsAdminSidebarMobileToggle.addEventListener('click', function (event) {
-            event.preventDefault();
-            event.stopPropagation();
-            if (!bsAdminSidebar.classList.contains('bs-admin-sidebar-open')) {
-                bsAdminSidebar.classList.add('bs-admin-sidebar-open')
+
+
+        window.addEventListener('scroll', function (e) {
+            //获取滚动条高度
+            let originScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+            //查找该元素是否能找到
+            let fdsf = document.querySelector('.back-to-top');
+
+            if (originScrollTop === 0) {
+                //清空定时器
+                clearInterval(timer);
+                enableClick = true;
+            } else if (originScrollTop > 300) {
+                fdsf !== null ? fdsf.style.display = 'block' : '';
             } else {
-                bsAdminSidebar.classList.remove('bs-admin-sidebar-open')
+                fdsf !== null ? fdsf.style.display = 'none' : '';
             }
-            let mask = document.querySelector(`.${bsAdminMaskClassName}`);
-            if (!mask) {
-                bsAdminMain.insertAdjacentHTML('beforeEnd', `<div class="${bsAdminMaskClassName}"></div>`);
-            } else {
-                mask.remove();
-            }
-            bsAdminNavbarCollapse.hide();
+
         });
-        bsAdminNavbarMobileToggle.addEventListener('click', function (event) {
+
+
+        // $(window).on("scroll", function () {
+        //     $(this).scrollTop() > 300 ? $(".back-to-top").fadeIn() : $(".back-to-top").fadeOut()
+        // });
+
+
+        // $(".back-to-top").on("click", function () {
+        //     return $("html, body").animate({
+        //         scrollTop: 0
+        //     }, 600);
+        // })
+
+
+        eventDelegate(document.querySelector('.bsa-sidebarcolor-wrap'), 'click', 'div[class^=sidebarcolor]', function (event) {
+
             event.preventDefault();
             event.stopPropagation();
-            bsAdminNavbarCollapse.toggle();
-            if (bsAdminSidebar) {
-                bsAdminSidebar.classList.remove('bs-admin-sidebar-open');
-            }
 
-            let mask = document.querySelector('.bs-admin-mask');
-            if (mask) {
-                mask.remove();
-            }
+            let sidebarcolor = this.getAttribute('class');
+            document.documentElement.classList.add("color-sidebar", sidebarcolor);
+
+            let sidebarcolorList = ["sidebarcolor1", "sidebarcolor2", "sidebarcolor3",
+                "sidebarcolor4", "sidebarcolor5", "sidebarcolor6",
+                "sidebarcolor7", "sidebarcolor8"];
+            sidebarcolorList.splice(sidebarcolorList.indexOf(sidebarcolor), 1);
+            document.documentElement.classList.remove(...sidebarcolorList);
         });
 
-        eventDelegate(bodyEl, 'click', '.bs-admin-mask', function (event) {
+
+        eventDelegate(document.querySelector('.bsa-headercolor-wrap'), 'click', 'div[class^=headercolor]', function (event) {
+
+            event.preventDefault();
+            event.stopPropagation();
+
+            let headercolor = this.getAttribute('class');
+            document.documentElement.classList.add("color-header", headercolor);
+            let headercolorList = ["headercolor1", "headercolor2", "headercolor3",
+                "headercolor4", "headercolor5", "headercolor6",
+                "headercolor7", "headercolor8"];
+            headercolorList.splice(headercolorList.indexOf(headercolor), 1);
+            document.documentElement.classList.remove(...headercolorList);
+        });
+
+
+        document.querySelector('.bsa-switcher-header>.btn-close').addEventListener('click', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+
+
+            let bsaSwitcherWrapper = document.querySelector('.bsa-switcher-wrapper');
+            bsaSwitcherWrapper.classList.remove('open');
+        });
+
+        //
+
+        document.querySelector('.bsa-switcher-btn').addEventListener('click', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+
+
+            let bsaSwitcherWrapper = document.querySelector('.bsa-switcher-wrapper');
+
+            if (!bsaSwitcherWrapper.classList.contains('open')) {
+                bsaSwitcherWrapper.classList.add('open')
+            } else {
+                bsaSwitcherWrapper.classList.remove('open')
+            }
+
+        });
+
+
+        //搜索框关闭
+        document.querySelector('.search-bar-box > i:nth-of-type(2)').addEventListener('click', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            //关闭搜索框
+            this.parentNode.parentNode.classList.remove('full-search-bar');
+        });
+
+        //搜索打开
+        document.querySelector('.bsm-mobile-search-icon').addEventListener('click', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            document.querySelector('.search-bar').classList.add('full-search-bar');
+
+
+        });
+
+
+        //遮罩层处理
+        eventDelegate(document.body, 'click', '.bsm-mask', function (event) {
             event.preventDefault();
             event.stopPropagation();
             this.remove();
-            if (!bsAdminSidebar.classList.contains('bs-admin-sidebar-open')) {
-                bsAdminSidebar.classList.add('bs-admin-sidebar-open')
-            } else {
-                bsAdminSidebar.classList.remove('bs-admin-sidebar-open')
-            }
-        })
+            unLockScroll();
+            sidebarEl.classList.remove('open');
+        });
+
+
+        //左侧菜单
+        eqwewqweq.addEventListener('click', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            sidebarEl.classList.add('open');
+            lockScroll();
+            document.body.insertAdjacentHTML('beforeEnd', `<div class="${msk}"></div>`);
+
+        });
+
 
         bsAdminSidebarWrapper.addEventListener("transitionend", function (event) {
             let target = event.target;
@@ -173,30 +279,6 @@
             })
         });
 
-
-        eventDelegate(bsAdminHeaderColorWrap, 'click', 'div[class^=headercolor]', function (e) {
-            let headercolor = this.getAttribute('class');
-            document.documentElement.classList.add("color-header", headercolor);
-            let headercolorList = ["headercolor1", "headercolor2", "headercolor3",
-                "headercolor4", "headercolor5", "headercolor6",
-                "headercolor7", "headercolor8"];
-            headercolorList.splice(headercolorList.indexOf(headercolor), 1);
-            document.documentElement.classList.remove(...headercolorList);
-        });
-
-
-        eventDelegate(bsAdminSidebarColorWrap, 'click', 'div[class^=sidebarcolor]', function (e) {
-            let sidebarcolor = this.getAttribute('class');
-
-
-            document.documentElement.classList.add("color-sidebar", sidebarcolor);
-
-            let sidebarcolorList = ["sidebarcolor1", "sidebarcolor2", "sidebarcolor3",
-                "sidebarcolor4", "sidebarcolor5", "sidebarcolor6",
-                "sidebarcolor7", "sidebarcolor8"];
-            sidebarcolorList.splice(sidebarcolorList.indexOf(sidebarcolor), 1);
-            document.documentElement.classList.remove(...sidebarcolorList);
-        });
 
     });
 })();
