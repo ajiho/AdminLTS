c61dc1c80b57452bc8153e261e6e1ab3ca14a508
x852 <lujiahao@88.com>
2022-08-10 23:33:45 +0800

    V1.0.1 Released!

--- README.md
+++ README.md
@@ -7,19 +7,21 @@
     <b>如果对您有所帮助，您可以点右上角 "Star" 收藏一下 ，获取第一时间更新，谢谢！</b>
 </p>
 
-
 ## 介绍
 
-基于bootstrap5.1.x设计的一个静态后台管理HTML模板，旨在快速让喜欢用bootstrap开发的后台程序员有个愉悦的起点。
+基于bootstrap5.1.x设计的一个静态后台管理HTML模板，它是开源免费的,旨在快速让喜欢用bootstrap开发的后台程序员有个愉悦的起点。
 
 演示地址:
-[V1.0.0](https://ajiho.gitee.io/bootstrap-admin)
+[V1.0.1](https://ajiho.gitee.io/bootstrap-admin)
+
+> Tips:如果没变化请使用ctrl+F5强制刷新查看
 
 ## 特性
 
 - 高度响应式、一站匹配手机、平板、pc
-- 整体白色为基调、简约、大气
+- 界面整体白色为基调、简约、大气、流畅不卡顿
 - 集成开发中常用的优秀js插件
+- 不依赖Jquery(集成的开源js插件目前还依赖Jquery的情况除外)
 
 ## 设备支持
 
@@ -36,6 +38,14 @@ iOS >= 12
 Safari >= 12
 not Explorer <= 11
 ~~~
+
+## 下载
+
+
+### 仓库下载发行版
+
+[发行版下载](https://gitee.com/ajiho/bootstrap-admin/releases)
+
 
 ## 反馈
 
--- src/bootstrap-admin.js
+++ src/bootstrap-admin.js
@@ -2,10 +2,177 @@
     window.addEventListener("DOMContentLoaded", function () {
 
 
+
+        //禁止无效表单提交
+        const invalidForms = document.querySelectorAll('form:not(form[action]),form[action="#"]');
+        for (const form of invalidForms) {
+            form.addEventListener('submit', event => {
+                event.preventDefault();
+            });
+        }
+
+
+
+
+        //禁止所有的input记忆
+        document.querySelectorAll('input').forEach((e, i) => {
+            e.setAttribute("AutoComplete", "off");
+        });
+        //全选
+        eventDelegate(document.body, 'change', '.check-all', function (event) {
+            let target = event.target;
+            let pTable = getParent(target, 'table')[0];
+            let checkboxes = pTable.querySelectorAll("input[type='checkbox']");
+            let status = target.checked;
+            checkboxes.forEach((el) => {
+                el.checked = status;
+            })
+        });
+
+        //调色板侧边栏
+        eventDelegate(document.body, 'click', '.bsa-sidebarcolor-wrap div[class^=sidebarcolor]', function (event) {
+
+            event.preventDefault();
+            event.stopPropagation();
+
+            let sidebarcolor = this.getAttribute('class');
+
+            let sidebarcolorList = ["sidebarcolor1", "sidebarcolor2", "sidebarcolor3",
+                "sidebarcolor4", "sidebarcolor5", "sidebarcolor6",
+                "sidebarcolor7", "sidebarcolor8"];
+
+            if (sidebarcolor === "sidebarcolor0") {
+                document.documentElement.classList.remove("color-sidebar");
+                sidebarcolorList.map(function (item) {
+                    document.documentElement.classList.remove(item);
+                });
+            } else {
+                document.documentElement.classList.add("color-sidebar", sidebarcolor);
+
+                sidebarcolorList.splice(sidebarcolorList.indexOf(sidebarcolor), 1);
+                document.documentElement.classList.remove(...sidebarcolorList);
+            }
+
+        });
+
+        //调色板头部
+        eventDelegate(document.body, 'click', '.bsa-headercolor-wrap div[class^=headercolor]', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            let headercolor = this.getAttribute('class');
+            let headercolorList = ["headercolor1", "headercolor2", "headercolor3",
+                "headercolor4", "headercolor5", "headercolor6",
+                "headercolor7", "headercolor8"];
+
+            if (headercolor === "headercolor0") {
+
+                document.documentElement.classList.remove("color-header");
+                headercolorList.map(function (item) {
+                    document.documentElement.classList.remove(item);
+                });
+
+            } else {
+                document.documentElement.classList.add("color-header", headercolor);
+                headercolorList.splice(headercolorList.indexOf(headercolor), 1);
+                document.documentElement.classList.remove(...headercolorList);
+            }
+
+
+        });
+
+
+        //调色板打开
+        eventDelegate(document.body, 'click', '.bsa-switcher-toggler-btn', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            let bsaSwitcher = document.querySelector('.bsa-switcher');
+            if (!bsaSwitcher.classList.contains('open')) {
+                bsaSwitcher.classList.add('open')
+            } else {
+                bsaSwitcher.classList.remove('open')
+            }
+        });
+
+
+        //搜索框关闭
+        eventDelegate(document.body, 'click', '.bsa-search-close-icon', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            document.querySelector('.bsa-search-form-item').classList.remove('open');
+        });
+
+
+        //搜索框打开
+        eventDelegate(document.body, 'click', '.bsa-mobile-search-toggler-item', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            document.querySelector('.bsa-search-form-item').classList.add('open');
+        });
+
+
+        //遮罩层点击
+        eventDelegate(document.body, 'click', '.bsa-mask', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            this.remove();
+            unLockScroll();
+            document.querySelector('.bsa-sidebar').classList.remove('open');
+        });
+
+
+        //移动端左侧菜单Toggler
+        eventDelegate(document.body, 'click', '.bsa-mobile-sidebar-toggler-item', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            document.querySelector('.bsa-sidebar').classList.add('open');
+            lockScroll();
+            document.body.insertAdjacentHTML('beforeEnd', `<div class="bsa-mask"></div>`);
+        });
+
+
+        eventDelegate(document.body, 'transitionend', '.bsa-sidebar-body .bsa-menu', function (event) {
+            let target = event.target;
+            if (target.matches('ul')) {
+                target.style = '';
+            }
+        });
+
+
+        //左侧菜单展开折叠逻辑
+        eventDelegate(document.body, 'click', '.bsa-menu a.has-children', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            let el_a_has_children = this;
+
+            if (!el_a_has_children.classList.contains('open')) {//展开
+                el_a_has_children.classList.add('open');
+                ulOpen(el_a_has_children);
+            } else {
+                ulClose(el_a_has_children);
+            }
+
+            // 兄弟节点处理
+            siblings(el_a_has_children.parentNode).forEach((el_li) => {
+                Array.from(el_li.children).forEach((item) => {
+
+
+                    if (item.matches('a.active')) {
+                      item.classList.remove('active');
+                    }
+                    if (item.matches('a.has-children.open')) {
+
+                        ulClose(item);
+                    }
+                })
+            })
+        });
+
         // 给每一个组件div都增加一个查看源码的按钮
         const bsaComponents = document.querySelectorAll('.bsa-component');
         for (const element of bsaComponents) {
-            const button = `<button class="bsa-source-button btn btn-primary shadow btn-xs" type="button" tabindex="0"><i class="bi bi-code"></i></button>`;
+            const button = `<button class="bsa-source-button btn bsa-btn-success btn-xs"><i class="bi bi-code"></i></button>`;
             element.insertAdjacentHTML('beforeend', button);
         }
 
@@ -14,13 +181,16 @@
             event.preventDefault();
             if (navigator.clipboard) {
                 const code = document.querySelector('#bsa-source-modal').querySelector('.modal-body pre').innerText;
-                navigator.clipboard.writeText(code);
+                navigator.clipboard.writeText(code).then(r => console.log('复制失败'));
             }
             bootstrap.Modal.getOrCreateInstance(document.querySelector('#bsa-source-modal')).hide();
         });
 
+
         //查看源码按钮点击事件
         eventDelegate(document.body, 'click', '.bsa-source-button', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
             //1.往body中插入展示代码的模态框，先判断是否存在这个模态框
             const sourceModalHtml = `
             <div id="bsa-source-modal" class="modal fade" tabindex="-1">
@@ -28,7 +198,10 @@
                     <div class="modal-content">
                         <div class="modal-header">
                             <h4 class="modal-title">源代码</h4>
-                            <button type="button" class="btn btn-primary bsa-btn-copy"><i class="bi bi-clipboard"></i>复制</button>
+                            <div class="d-flex align-items-center flex-nowrap gap-2">
+                                <button type="button" class="btn bsa-btn-success bsa-btn-copy"><i class="bi bi-clipboard me-1"></i>复制</button>
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" aria-label="Close"><i class="bi bi-x-lg"></i></button>
+                            </div>
                         </div>
                         <div class="modal-body">
                             <pre class="language-html"><code></code></pre>
@@ -53,174 +226,64 @@
         });
 
 
-        //禁止所有的input记忆
-        document.querySelectorAll('input').forEach((e, i) => {
-            e.setAttribute("AutoComplete", "off");
+        //bootstrap气泡组件初始化
+        [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]')).map(function (popoverTriggerEl) {
+            return new bootstrap.Popover(popoverTriggerEl)
         });
 
 
-        //全选
-        eventDelegate(document.body, 'change', '.check-all', function (event) {
-            let target = event.target;
-            let pTable = getParent(target, 'table')[0];
-            let checkboxes = pTable.querySelectorAll("input[type='checkbox']");
-            let status = target.checked;
-            checkboxes.forEach((el, index) => {
-                el.checked = status;
-            })
-        });
-
-        eventDelegate(document.body, 'click', '.bsa-sidebarcolor-wrap div[class^=sidebarcolor]', function (event) {
-
-            event.preventDefault();
-            event.stopPropagation();
-
-            let sidebarcolor = this.getAttribute('class');
-            document.documentElement.classList.add("color-sidebar", sidebarcolor);
-
-            let sidebarcolorList = ["sidebarcolor1", "sidebarcolor2", "sidebarcolor3",
-                "sidebarcolor4", "sidebarcolor5", "sidebarcolor6",
-                "sidebarcolor7", "sidebarcolor8"];
-            sidebarcolorList.splice(sidebarcolorList.indexOf(sidebarcolor), 1);
-            document.documentElement.classList.remove(...sidebarcolorList);
-        });
-
-
-        eventDelegate(document.body, 'click', '.bsa-headercolor-wrap div[class^=headercolor]', function (event) {
-            event.preventDefault();
-            event.stopPropagation();
-
-            let headercolor = this.getAttribute('class');
-            document.documentElement.classList.add("color-header", headercolor);
-            let headercolorList = ["headercolor1", "headercolor2", "headercolor3",
-                "headercolor4", "headercolor5", "headercolor6",
-                "headercolor7", "headercolor8"];
-            headercolorList.splice(headercolorList.indexOf(headercolor), 1);
-            document.documentElement.classList.remove(...headercolorList);
-        });
-
-
-        //调色板打开
-        eventDelegate(document.body, 'click', '.bsa-switcher .bsa-switcher-toggler-btn', function (event) {
-            event.preventDefault();
-            event.stopPropagation();
-
-            let bsaSwitcher = document.querySelector('.bsa-switcher');
-            if (!bsaSwitcher.classList.contains('open')) {
-                bsaSwitcher.classList.add('open')
-            } else {
-                bsaSwitcher.classList.remove('open')
-            }
-        });
-
-
-        //搜索框关闭
-        eventDelegate(document.body, 'click', '.bsa-search-box .bsa-search-close-icon', function (event) {
-            event.preventDefault();
-            event.stopPropagation();
-            document.querySelector('.bsa-search-bar-item').classList.remove('open');
-        });
-
-
-        //搜索框打开
-        eventDelegate(document.body, 'click', '.bsa-mobile-search-item', function (event) {
-            event.preventDefault();
-            event.stopPropagation();
-            document.querySelector('.bsa-search-bar-item').classList.add('open');
-        });
-
-
-        //遮罩层点击
-        eventDelegate(document.body, 'click', '.bsa-mask', function (event) {
-            event.preventDefault();
-            event.stopPropagation();
-            this.remove();
-            unLockScroll();
-            document.querySelector('.bsa-sidebar').classList.remove('open');
-        });
-
-
-        //左侧菜单
-        eventDelegate(document.body, 'click', '.bsa-header .bsa-mobile-sidebar-item', function (event) {
-            event.preventDefault();
-            event.stopPropagation();
-            document.querySelector('.bsa-sidebar').classList.add('open');
-            lockScroll();
-            document.body.insertAdjacentHTML('beforeEnd', `<div class="bsa-mask"></div>`);
-        });
-
-
-        eventDelegate(document.body, 'transitionend', '.bsa-sidebar-body .bsa-menu', function (event) {
-            let target = event.target;
-            if (target.nodeName.toLowerCase() === 'ul') {
-                target.style = '';
-            }
-        });
-
-        eventDelegate(document.body, 'click', '.bsa-sidebar-body .bsa-menu a.has-children', function (event) {
-            event.preventDefault();
-            event.stopPropagation();
-            let el = this;
-            if (!el.classList.contains('open')) {//展开
-                el.classList.add('open');
-                //先计算得到它原本的高度
-                let ulHeight = el.nextElementSibling.scrollHeight + 'px';
-                //1.动态设置高度为0
-                el.nextElementSibling.style.cssText = `display:block;height:0;overflow: hidden;`;
-                void el.scrollHeight;
-                el.nextElementSibling.style.cssText = `display:block;height:${ulHeight};overflow: hidden;`;
-            } else {
-                let ulHeight = el.nextElementSibling.scrollHeight + 'px';
-                el.classList.remove('open');
-                //1.动态设置高度
-                el.nextElementSibling.style.cssText = `display:block;height:${ulHeight};overflow: hidden;`;
-                //重新读取属性让css重排
-                void el.scrollHeight;
-                //2.设置为0
-                el.nextElementSibling.style.cssText = `display:block;height:0;overflow: hidden;`;
-            }
-
-            // 兄弟节点处理
-            let pNode = el.parentNode;
-            let sbs = siblings(pNode);
-            sbs.forEach((el, index) => {
-                Array.from(el.children).forEach((el, index) => {
-                    if (el.matches('a.has-children.open')) {
-                        let ulHeight = el.nextElementSibling.scrollHeight + 'px';
-                        el.classList.remove('open');
-                        //1.动态设置高度
-                        el.nextElementSibling.style.cssText = `display:block;height:${ulHeight};overflow: hidden;`;
-                        //重新读取属性让css重排
-                        void el.scrollHeight;
-                        //2.设置为0
-                        el.nextElementSibling.style.cssText = `display:block;height:0;overflow: hidden;`;
-                    } else if (el.matches('a.active')) {
-                        el.classList.remove('active');
-                    }
-                })
-            })
+        //bootstrap提示组件初始化
+        [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]')).map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl)
         });
 
 
         //============================助手函数============================
-
-        let scrollTop;
-
-        function lockScroll() {
-            scrollTop = document.scrollingElement.scrollTop;
-            document.body.classList.add('bsa-lock-scroll');
-            document.body.style.top = -scrollTop + 'px';
+        function ulOpen(dom) {
+            //找到ul元素
+            let ul = dom.nextElementSibling;
+            //先计算得到ul它原本的高度
+            let ulHeight = ul.scrollHeight + 'px';
+            ul.style.cssText = `display:block;height:0;overflow: hidden;`;
+            void dom.scrollHeight;
+            ul.style.cssText = `display:block;height:${ulHeight};overflow: hidden;`;
         }
 
+        function ulClose(dom) {
+            //找到ul元素
+            let ul = dom.nextElementSibling;
+            //先计算得到ul它原本的高度
+            let ulHeight = ul.scrollHeight + 'px';
+            ul.style.cssText = `display:block;height:${ulHeight};overflow: hidden;`;
+            void dom.scrollHeight;
+            ul.style.cssText = `display:block;height:0;overflow: hidden;`;
+            dom.classList.remove('open');
+        }
+
+
+        function lockScroll() {
+            if (hasScrolled(document.documentElement, 'vertical')) {//如果有滚动条就保持滚动条
+                document.body.classList.add('bsa-lock-body-has-scroll');
+            } else {
+                document.body.classList.add('bsa-lock-body-no-scroll');
+            }
+            let st = document.scrollingElement.scrollTop;
+            window.localStorage.setItem("bsa-scroll-top", st.toString())
+            document.body.classList.add('bsa-lock-body');
+            document.body.style.top = -st + 'px';
+
+
+        }
+
+
         function unLockScroll() {
-            document.body.classList.remove('bsa-lock-scroll');
-            document.scrollingElement.scrollTop = scrollTop;
+            document.body.classList.remove(...['bsa-lock-body', 'bsa-lock-body-has-scroll', 'bsa-lock-body-no-scroll']);
+            document.scrollingElement.scrollTop = Number(window.localStorage.getItem("bsa-scroll-top"));
         }
 
         function siblings(el) {
             let a = [];    //保存所有兄弟节点
             let p = el.parentNode.children; //获取父级的所有子节点
-
             for (let i = 0; i < p.length; i++) {  //循环
                 if (p[i].nodeType === 1 && p[i] !== el) {  //如果该节点是元素节点与不是这个节点本身
                     a.push(p[i]);      // 添加到兄弟节点里
@@ -278,8 +341,7 @@
         }
 
         function cleanSource(html) {
-            // Escape HTML, split the lines to an Array, remove empty elements
-            // and finally remove the last element
+            //转义HTML,将行拆分为数组,删除空元素,并移除最后一个元素
             let lines = escapeHtml(html).split('\n').filter(Boolean).slice(0, -1);
             const indentSize = lines[0].length - lines[0].trim().length;
             const re = new RegExp(' {' + indentSize + '}');
