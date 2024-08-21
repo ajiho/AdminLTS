f1a9ef4a416f3755065d2bf938379e9ae8d3d4a4
ajiho <lujiahao@88.com>
2024-08-21 15:08:54 +0800

    chore: 调整bundlewatch配置文件目录
    Signed-off-by: ajiho <lujiahao@88.com>

---
+++ src/config/bundlewatch.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  files: [
+    {
+      path: './dist/js/adminlts.js',
+      maxSize: '40 KB',
+    },
+    {
+      path: './dist/css/adminlts.css',
+      maxSize: '45 KB',
+    },
+  ],
+  ci: {
+    trackBranches: ['master', 'dev'],
+  },
+}
